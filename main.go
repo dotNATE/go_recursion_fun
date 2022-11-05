2e5d0ee6a5efdf2f417517f5a5193ac3d8a8adc0
dotNate <somersetnathaniel@gmail.com>
2022-11-05 20:56:06 +0000

    print result in main.go

--- main.go
+++ main.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	myHolding := newHolding("Ethical Global Fund", 1)
 	fundData := getFundDataFromDir()
 
-	fmt.Printf("fund data: %+v\n", fundData)
+	myCompanies := extractCompaniesFromFundDataRecursively(fundData, myHolding)
+	var totalWeight float32
+
+	fmt.Print("Your holdings:\n\n")
+
+	for _, holding := range myCompanies {
+		totalWeight += holding.Weight
+		fmt.Printf("Name: %s Weight: %.2f \n", holding.Name, holding.Weight)
+	}
+
+	fmt.Printf("\nTotal Weight: %f", totalWeight)
 }

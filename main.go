e7a8a2be0b549fe536b4cdeb5c8ecb9c1fc94fcc
dotNate <somersetnathaniel@gmail.com>
2022-11-12 11:11:54 +0000

    updated main.go to take user input

--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	myHolding := NewHolding("Ethical Global Fund", 1)
 	fundData := GetFundDataFromDir()
-
 	myCompanies := ExtractCompaniesFromFundDataRecursively(fundData, myHolding)
-	var totalWeight float32
 
-	fmt.Print("Your holdings:\n\n")
-
+	fmt.Print("\nYour holdings:\n\n")
 	for _, holding := range myCompanies {
-		totalWeight += holding.Weight
 		fmt.Printf("Name: %s Weight: %.2f \n", holding.Name, holding.Weight)
 	}
 
-	fmt.Printf("\nTotal Weight: %f", totalWeight)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("\nHow much do you have invested?")
+	var investmentValue float32
+	fmt.Scanln(&investmentValue)
+
+	fmt.Println("\nWhich company do you want to check?")
+	scanner.Scan()
+	holdingName := scanner.Text()
+
+	n := FindHoldingInSlice(myCompanies, holdingName)
+	holdingExists := n > -1
+	if !holdingExists {
+		fmt.Println("\nCompany not found")
+		os.Exit(1)
+	}
+
+	fmt.Printf("\nInvestment Value: £%.2f", investmentValue)
+	fmt.Printf("\nHolding Name: %s", holdingName)
+
+	for _, holding := range myCompanies {
+		if holding.Name == holdingName {
+			fmt.Printf("\nHolding Value: £%.2f", investmentValue*holding.Weight)
+		}
+	}
 }
